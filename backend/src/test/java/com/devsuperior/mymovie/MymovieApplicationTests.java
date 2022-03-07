1290c8e04191e4c5f228208dc40aca0483b0389f
Lucasandrade52 <lucasandrade_52@hotmail.com>
2022-03-07 15:36:06 -0300

    create project

---
+++ backend/src/test/java/com/devsuperior/mymovie/MymovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.mymovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
